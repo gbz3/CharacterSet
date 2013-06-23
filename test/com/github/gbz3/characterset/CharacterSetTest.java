5e979540d56d50d788345ed9534348a2f19739ab
gbz3 <gebizo3@gmail.com>
2013-06-23 16:35:38 +0900

    JIS X 0201 JIS X 0208

--- test/com/github/gbz3/characterset/CharacterSetTest.java
+++ test/com/github/gbz3/characterset/CharacterSetTest.java
@@ -1,14 +1,30 @@
 package com.github.gbz3.characterset;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class CharacterSetTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testJISX0201() {
+		final CharacterSet set = CharacterSet.getInstance();
+		assertEquals( false, set.is( '\u0019', CharacterSet.Enable.JISX0201 ) );
+		assertEquals( true, set.is( ' ', CharacterSet.Enable.JISX0201 ) );
+		assertEquals( true, set.is( '}', CharacterSet.Enable.JISX0201 ) );
+		assertEquals( true, set.is( '\u203E', CharacterSet.Enable.JISX0201 ) );		// OVER LINE
+		assertEquals( false, set.is( '\u007F', CharacterSet.Enable.JISX0201 ) );
+		assertEquals( true, set.is( '｡', CharacterSet.Enable.JISX0201 ) );
+		assertEquals( true, set.is( 'ﾟ', CharacterSet.Enable.JISX0201 ) );
+	}
+
+	@Test
+	public void testJISX0208() {
+		final CharacterSet set = CharacterSet.getInstance();
+		assertEquals( false, set.is( '1', CharacterSet.Enable.JISX0208 ) );
+		assertEquals( true, set.is( '１', CharacterSet.Enable.JISX0208 ) );
+		assertEquals( false, set.is( '①', CharacterSet.Enable.JISX0208 ) );
+		assertEquals( false, set.is( '～', CharacterSet.Enable.JISX0208 ) );
 	}
 
 }
